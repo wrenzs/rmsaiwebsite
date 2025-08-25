5915d465bf1a927ab4b71151ab3d47970aca5ed6
wrenzs <90405264+wrenzs@users.noreply.github.com>
2025-08-25 13:09:21 +0800

    Add files via upload
    "Initial upload of website files"

---
+++ script.js
@@ -0,0 +1,9 @@
+// Navbar scroll effect
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 50) {
+        navbar.classList.add('navbar-scrolled');
+    } else {
+        navbar.classList.remove('navbar-scrolled');
+    }
+});
